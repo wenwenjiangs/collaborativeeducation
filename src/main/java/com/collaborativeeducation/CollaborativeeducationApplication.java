f56c463493da5e57adde70514c098e64ee4ffbef
wenwenjiangs <957590651@qq.com>
2019-01-22 20:39:03 +0800

    education

---
+++ src/main/java/com/collaborativeeducation/CollaborativeeducationApplication.java
@@ -0,0 +1,14 @@
+package com.collaborativeeducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeeducationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollaborativeeducationApplication.class, args);
+    }
+
+}
+
